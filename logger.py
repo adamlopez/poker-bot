be3f718a5d6dbb77de2450edc6bd0e09c1e78f3e
ehlopez <adam.lopez@carleton.ca>
2018-10-09 10:20:44 -0400

    remove deprecated time.clock() calls

--- logger.py
+++ logger.py
@@ -1,22 +1,22 @@
 import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-import pandas as pd
 import logging
+import datetime as dt
 
 def getLogger():
     textLogger = logging.getLogger('logs/')
     textLogger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
 
-    file = f'{textLogger.name}/{pd.Timestamp.now().date()}.log' if os.path.isdir(textLogger.name) else f'{pd.Timestamp.now().date()}.log'
+    file = f'{textLogger.name}/{dt.datetime.now().date()}.log' if os.path.isdir(textLogger.name) else f'{dt.datetime.now().date()}.log'
     fh = logging.FileHandler(file)
     fh.setLevel(logging.DEBUG) #file stores everything logged
     fh.setFormatter(formatter)
     textLogger.addHandler(fh)
 
     ch = logging.StreamHandler() #lets you print to display while logging
-    ch.setLevel(logging.INFO) # everything other than debug level msgs will be displayed on console
+    ch.setLevel(logging.WARNING) # everything other than debug level msgs will be displayed on console
     ch.setFormatter(formatter)
     textLogger.addHandler(ch)
     return textLogger
