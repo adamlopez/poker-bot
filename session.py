2e4b2b7d33920ced5aba0586020aa8c7a8dd958b
ehlopez <adam.lopez@carleton.ca>
2018-10-09 10:21:54 -0400

    fixed/automated testing, started on kicker logic

--- session.py
+++ session.py
@@ -1,12 +1,16 @@
-from person import Card, Player
+from objects import Card, Player
 import itertools
 import time
 from random import shuffle
 from collections import deque
-
+from typing import List, Dict
 SUITS = ['H','D','S','C']
+SUIT_MAP = {'H' : 'Hearts',
+            'D' : 'Diamonds',
+            'S' : 'Spades',
+            'C' : 'Clubs'}
 CARDS = [i for i in range(2,15)]
-HAND_RANKS = {'royal flush' : 1,
+HAND_RANKS = {'royal flush' : 1, #keep these sorted by best to worst hand
               'straight flush' : 2,
               'quads' : 3,
               'full house':4,
@@ -17,18 +21,16 @@ HAND_RANKS = {'royal flush' : 1,
               'pair':9,
               'high card':10}
 
-
 class Session:
     ALL_CARDS = []
-
     for i in range(1,14):
         for suit in SUITS:
             ALL_CARDS.append(Card(suit,i))
     POSSIBLE_TABLES = list(itertools.combinations(ALL_CARDS,5))
 
-    def __init__(self,logger,playerCount=2):
-        self.logger = logger
-        self.logger.info('New session started.')
+    def __init__(self,log,playerCount=2):
+        self.log = log
+        self.log.info('New session started.')
         self.COMMUNITY_CARDS = []
         self.deck = deque(self.ALL_CARDS)
         self.players = []
@@ -39,16 +41,16 @@ class Session:
     def getGameOutcome(self):
         outcomes = []
 
-    def showState(self):
+    def showState(self) -> None:
         for player in self.players:
-            self.logger.info(f"{player.name}'s hand: {player.hand}")
-        self.logger.info(f"Community Cards: {self.COMMUNITY_CARDS}")
+            self.log.info(f"{player.name}'s hand: {player.hand}")
+        self.log.info(f"Community Cards: {self.COMMUNITY_CARDS}")
 
 
     def newGame(self):
         '''starts a new game by resetting all Session attributes as needed.'''
         self.COMMUNITY_CARDS = []
-        self.logger.info('shuffling...')
+        self.log.info('shuffling...')
         shuffle(self.deck)
         #assign cards to players hands
         for player in self.players:
@@ -58,28 +60,41 @@ class Session:
         for i in range(5):
             self.COMMUNITY_CARDS.append(self.deck.pop())
 
+    def getHighCards(self, player_num: int, num_cards) -> List[Card]:
+        """returns a list of the <num_cards> highest cards between player
+        <player_num> and the community cards.
+        """
+
+        pass
+
 
     def getBestHands(self):
-        ''' returns a string representation of the best hands each player holds.'''
+        ''' returns a string representation of the best hands
+            each player holds.'''
         results = {}
         for player in self.players:
-            kickers = dict(zip(HAND_RANKS.keys(),[[None for i in range(3)] for k in range(len(HAND_RANKS.keys()))]))
+            item = [None for i in range(5)]
+            rng = range(len(HAND_RANKS.keys()))
+            kickers = dict(zip(HAND_RANKS.keys(),[item for k in rng]))
             cards = player.hand + self.COMMUNITY_CARDS
 
-            handcounts = dict(zip(HAND_RANKS.keys(),[0 for i in range(len(HAND_RANKS.keys()))]))
+            zeros = [0 for i in range(len(HAND_RANKS.keys()))]
+            handcounts = dict(zip(HAND_RANKS.keys(),zeros))
             suitcounts = dict(zip(SUITS,[0 for i in range(len(SUITS))]))
 
             #high card
-            sortedCards = [card.value for card in player.hand]
-            sortedCards.sort()
-            handcounts['high card'] = sortedCards[-1]
+            sortedCards = sorted(player.hand + self.COMMUNITY_CARDS,
+                                key=lambda card: card.value)
+             handcounts['high card'] = sortedCards[-1]
+            kickers['high card'] = sortedCards[-5:][::-1]
 
             # check for pair, trips and quads
             counted = []
             for baseCard in cards:
                 suitcounts[baseCard.suit] += 1
                 valCount = 1
-                withoutBase = [x for x in cards if x != baseCard] #return copy without the base card
+                #create copy of list with base card removed
+                withoutBase = [x for x in cards if x != baseCard]
                 for card in withoutBase:
                     if baseCard.value == card.value and baseCard not in counted:
                         valCount += 1
@@ -88,44 +103,44 @@ class Session:
                 if valCount == 2:
                     kickers['pair'][handcounts['pair']] = baseCard.value
                     handcounts['pair'] += 1
-                    self.logger.info("pair!")
+                    self.log.info(f"pair! ({player})")
                 elif valCount == 3:
                     kickers['trips'][handcounts['trips']] = baseCard.value
                     handcounts['trips'] += 1
-                    self.logger.info('trips!')
+                    self.log.info(f'trips! ({player})')
                 elif valCount == 4:
                     kickers['quads'][0] = baseCard.value
                     handcounts['quads'] += 1
                     kickers[0] = baseCard.value
-                    self.logger.info('quads!!')
+                    self.log.info(f'quads! ({player})')
 
             if handcounts['pair'] > 1:
                 handcounts['two pair'] += 1
                 kickers['two pair'] = kickers['pair']
-                self.logger.info('two pair!')
+                self.log.info(f'two pair! ({player})')
 
             if handcounts['pair'] >= 1 and handcounts['trips'] >= 1:
                 handcounts['full house'] += 1
                 kickers['full house'][0] = kickers['trips'][0]
                 kickers['full house'][1] = kickers['pair'][0]
                 print(kickers)
-                self.logger.info('full house!')
+                self.log.info(f'full house! ({player})')
 
             for v in suitcounts.values():
                 if v >= 5:
                     handcounts['flush'] += 1
-                    self.logger.info(f'flush!')
+                    self.log.info(f'flush! ({player})')
             sortedCards = sorted(cards,key=lambda card: card.value)
-            self.logger.debug(f'sorted cards: {sortedCards}')
+            self.log.debug(f'sorted cards: {sortedCards}')
             if sortedCards[-1].value == 14: #count ace as lowest and highest
                 sortedCards.insert(0,Card(sortedCards[-1].suit,1))
-                self.logger.debug(f'sorted cards post insert: {sortedCards}')
+                self.log.debug(f'sorted cards post insert: {sortedCards}')
             prevNum = -1
             suitStreak = 0
             streak = 1
             prevSuit = [sortedCards[0].suit]
             for card in sortedCards:
-                self.logger.debug(f'\ncard: {card}')
+                self.log.debug(f'\ncard: {card}')
                 if prevNum + 1 == card.value:
                     streak += 1
                     for suit in prevSuit:
@@ -143,24 +158,24 @@ class Session:
                     suitStreak = 1
                     prevSuit = [card.suit]
 
-                self.logger.debug(f'value streak: {streak}')
-                self.logger.debug(f'suit streak: {suitStreak}')
+                self.log.debug(f'value streak: {streak}')
+                self.log.debug(f'suit streak: {suitStreak}')
                 prevNum = card.value
-                self.logger.debug(f'prev suit: {prevSuit}')
+                self.log.debug(f'prev suit: {prevSuit}')
                 if streak >= 5 and  suitStreak >= 5:
                     if card.value == 14:
-                        self.logger.info(f'{card.suit} royal flush!!')
+                        self.log.info(f'{card.suit} royal flush! ({player})')
                         handcounts['royal flush'] = 1
                     else:
-                        self.logger.info(f'{card}-high straight flush!')
+                        self.log.info(f'{card}-high straight flush! ({player})')
                     handcounts['straight flush'] = 1
                 elif streak >= 5:
-                    self.logger.debug(f'{card}-high straight!')
+                    self.log.debug(f'{card}-high straight! ({player})')
                     handcounts['straight'] = 1
 
 
             for hand,freq in handcounts.items():
-                self.logger.info(f"{hand}:{freq}")
+                self.log.debug(f"{hand}:{freq}")
                 if freq > 0: #will automatically take the highest ranked hand
                     results[player] = (hand,kickers[hand])
                     break
--- tests.py
+++ tests.py
@@ -1,164 +1,233 @@
 import time
-start = time.clock()
-from person import Card,Player
+start = time.perf_counter()
+from objects import Card, Player
 from session import Session
 from logger import getLogger
 import sys
+import pytest
 logger = getLogger()
 
-def runTest(testCode):
-    logger.info(f'------------------ TEST {testCode} ------------------')
+def test_high_card():
     sesh = Session(logger,playerCount=2)
     players = ['Adam','Verina']
     for i in range(len(players)):
         sesh.players[i].name = players[i]
     sesh.newGame()
+    sesh.players[0].hand[0] = Card('H',12)
+    sesh.players[0].hand[1] = Card('S',14)
 
-    if testCode == 1: #full house and pair  test
-        sesh.players[0].hand[0] = Card('H',12)
-        sesh.players[0].hand[1] = Card('S',12)
+    sesh.players[1].hand[0] = Card('C',2)
+    sesh.players[1].hand[1] = Card('D',6)
 
-        sesh.players[1].hand[0] = Card('C',10)
-        sesh.players[1].hand[1] = Card('D',8)
+    sesh.COMMUNITY_CARDS[0] = Card('D',10)
+    sesh.COMMUNITY_CARDS[1] = Card('S',9)
+    sesh.COMMUNITY_CARDS[2] = Card('C',5)
+    sesh.COMMUNITY_CARDS[3] = Card('D',3)
+    sesh.COMMUNITY_CARDS[4] = Card('C',13)
+    sesh.showState()
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'high card'
+    assert res[sesh.players[0]][1] == [14, 13, 12, 10, 9]
+    assert res[sesh.players[1]][0] == 'high card'
+    assert res[sesh.players[1]][1] == [13, 10, 9, 6, 5]
 
-        sesh.COMMUNITY_CARDS[0] = Card('D',10)
-        sesh.COMMUNITY_CARDS[1] = Card('S',10)
-        sesh.COMMUNITY_CARDS[2] = Card('C',8)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',12)
-    elif testCode == 2: # 2 pockets
-        sesh.players[0].hand[0] = Card('H',12)
-        sesh.players[0].hand[1] = Card('S',12)
 
-        sesh.players[1].hand[0] = Card('C',10)
-        sesh.players[1].hand[1] = Card('S',10)
 
-        sesh.COMMUNITY_CARDS[0] = Card('H',9)
-        sesh.COMMUNITY_CARDS[1] = Card('S',4)
-        sesh.COMMUNITY_CARDS[2] = Card('C',8)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',13)
+def test_full_house_pair():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('H',12)
+    sesh.players[0].hand[1] = Card('S',12)
 
-    elif testCode == 3: #flush test
-        sesh.players[0].hand[0] = Card('H',12)
-        sesh.players[0].hand[1] = Card('S',12)
+    sesh.players[1].hand[0] = Card('C',10)
+    sesh.players[1].hand[1] = Card('D',8)
 
-        sesh.players[1].hand[0] = Card('D',10)
-        sesh.players[1].hand[1] = Card('D',8)
+    sesh.COMMUNITY_CARDS[0] = Card('D',10)
+    sesh.COMMUNITY_CARDS[1] = Card('S',10)
+    sesh.COMMUNITY_CARDS[2] = Card('C',8)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('C',12)
+    sesh.showState()
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'full house'
+    assert res[sesh.players[1]][0] == 'full house'
 
-        sesh.COMMUNITY_CARDS[0] = Card('H',10)
-        sesh.COMMUNITY_CARDS[1] = Card('C',10)
-        sesh.COMMUNITY_CARDS[2] = Card('C',8)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',12)
-    elif testCode == 4:#straight test
-        sesh.players[0].hand[0] = Card('S',12)
-        sesh.players[0].hand[1] = Card('C',11)
+def test_pockets():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('H',12)
+    sesh.players[0].hand[1] = Card('S',12)
+    sesh.players[1].hand[0] = Card('C',10)
+    sesh.players[1].hand[1] = Card('S',10)
+    sesh.COMMUNITY_CARDS[0] = Card('H',9)
+    sesh.COMMUNITY_CARDS[1] = Card('S',4)
+    sesh.COMMUNITY_CARDS[2] = Card('C',8)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('C',13)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'pair'
+    assert res[sesh.players[1]][0] == 'pair'
 
-        sesh.players[1].hand[0] = Card('D',10)
-        sesh.players[1].hand[1] = Card('D',8)
+def test_flush_1():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('H',12)
+    sesh.players[0].hand[1] = Card('S',13)
+    sesh.players[1].hand[0] = Card('D',10)
+    sesh.players[1].hand[1] = Card('D',8)
 
-        sesh.COMMUNITY_CARDS[0] = Card('S',10)
-        sesh.COMMUNITY_CARDS[1] = Card('D',9)
-        sesh.COMMUNITY_CARDS[2] = Card('C',8)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',12)
-    elif testCode == 5: #regular straight test 1
-        sesh.players[0].hand[0] = Card('S',12)
-        sesh.players[0].hand[1] = Card('C',11)
+    sesh.COMMUNITY_CARDS[0] = Card('H',10)
+    sesh.COMMUNITY_CARDS[1] = Card('C',10)
+    sesh.COMMUNITY_CARDS[2] = Card('D',5)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('D',12)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'two pair'
+    assert res[sesh.players[1]][0] == 'flush'
 
-        sesh.players[1].hand[0] = Card('D',10)
-        sesh.players[1].hand[1] = Card('D',8)
+def test_straight_1():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('S',12)
+    sesh.players[0].hand[1] = Card('C',11)
+    sesh.players[1].hand[0] = Card('D',10)
+    sesh.players[1].hand[1] = Card('D',8)
+    sesh.COMMUNITY_CARDS[0] = Card('S',10)
+    sesh.COMMUNITY_CARDS[1] = Card('D',9)
+    sesh.COMMUNITY_CARDS[2] = Card('C',8)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('C',12)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'straight'
+    assert res[sesh.players[1]][0] == 'two pair'
 
-        sesh.COMMUNITY_CARDS[0] = Card('S',10)
-        sesh.COMMUNITY_CARDS[1] = Card('D',9)
-        sesh.COMMUNITY_CARDS[2] = Card('C',8)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',12)
-    elif testCode == 6: #ace high straight test
-        sesh.players[0].hand[0] = Card('S',14)
-        sesh.players[0].hand[1] = Card('C',2)
+def test_ace_high_straight():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('S',14)
+    sesh.players[0].hand[1] = Card('C',2)
+    sesh.players[1].hand[0] = Card('D',10)
+    sesh.players[1].hand[1] = Card('D',8)
+    sesh.COMMUNITY_CARDS[0] = Card('S',13)
+    sesh.COMMUNITY_CARDS[1] = Card('D',12)
+    sesh.COMMUNITY_CARDS[2] = Card('C',11)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('C',10)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'straight'
+    assert res[sesh.players[1]][0] == 'pair'
 
-        sesh.players[1].hand[0] = Card('D',10)
-        sesh.players[1].hand[1] = Card('D',8)
+def test_ace_low_straight():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('S',14)
+    sesh.players[0].hand[1] = Card('C',2)
+    sesh.players[1].hand[0] = Card('D',10)
+    sesh.players[1].hand[1] = Card('D',8)
+    sesh.COMMUNITY_CARDS[0] = Card('S',3)
+    sesh.COMMUNITY_CARDS[1] = Card('D',4)
+    sesh.COMMUNITY_CARDS[2] = Card('C',5)
+    sesh.COMMUNITY_CARDS[3] = Card('D',9)
+    sesh.COMMUNITY_CARDS[4] = Card('C',10)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'straight'
+    assert res[sesh.players[1]][0] == 'pair'
 
-        sesh.COMMUNITY_CARDS[0] = Card('S',13)
-        sesh.COMMUNITY_CARDS[1] = Card('D',12)
-        sesh.COMMUNITY_CARDS[2] = Card('C',11)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',10)
+def test_straight_flush_1():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('S',14)
+    sesh.players[0].hand[1] = Card('S',2)
+    sesh.players[1].hand[0] = Card('D',10)
+    sesh.players[1].hand[1] = Card('D',8)
+    sesh.COMMUNITY_CARDS[0] = Card('S',3)
+    sesh.COMMUNITY_CARDS[1] = Card('S',4)
+    sesh.COMMUNITY_CARDS[2] = Card('S',5)
+    sesh.COMMUNITY_CARDS[3] = Card('D',6)
+    sesh.COMMUNITY_CARDS[4] = Card('C',10)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'straight flush'
+    assert res[sesh.players[1]][0] == 'pair'
 
-    elif testCode == 7: # ace low straight test
-        sesh.players[0].hand[0] = Card('S',14)
-        sesh.players[0].hand[1] = Card('C',2)
+def test_straight_flush_2():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('D',10)
+    sesh.players[0].hand[1] = Card('D',8)
+    sesh.players[1].hand[0] = Card('C',6)
+    sesh.players[1].hand[1] = Card('C',2)
+    sesh.COMMUNITY_CARDS[0] = Card('C',3)
+    sesh.COMMUNITY_CARDS[1] = Card('C',4)
+    sesh.COMMUNITY_CARDS[2] = Card('C',5)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('C',10)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'pair'
+    assert res[sesh.players[1]][0] == 'straight flush'
 
-        sesh.players[1].hand[0] = Card('D',10)
-        sesh.players[1].hand[1] = Card('D',8)
+def test_royal_flush_1():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('D',10)
+    sesh.players[0].hand[1] = Card('D',12)
+    sesh.players[1].hand[0] = Card('C',6)
+    sesh.players[1].hand[1] = Card('C',2)
 
-        sesh.COMMUNITY_CARDS[0] = Card('S',3)
-        sesh.COMMUNITY_CARDS[1] = Card('D',4)
-        sesh.COMMUNITY_CARDS[2] = Card('C',5)
-        sesh.COMMUNITY_CARDS[3] = Card('D',6)
-        sesh.COMMUNITY_CARDS[4] = Card('C',10)
-    elif testCode == 8: # straight flush test
-        sesh.players[0].hand[0] = Card('S',14)
-        sesh.players[0].hand[1] = Card('S',2)
+    sesh.COMMUNITY_CARDS[0] = Card('D',14)
+    sesh.COMMUNITY_CARDS[1] = Card('D',11)
+    sesh.COMMUNITY_CARDS[2] = Card('D',2)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('D',13)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'royal flush'
+    assert res[sesh.players[1]][0] == 'flush'
 
-        sesh.players[1].hand[0] = Card('D',10)
-        sesh.players[1].hand[1] = Card('D',8)
-
-        sesh.COMMUNITY_CARDS[0] = Card('S',3)
-        sesh.COMMUNITY_CARDS[1] = Card('S',4)
-        sesh.COMMUNITY_CARDS[2] = Card('S',5)
-        sesh.COMMUNITY_CARDS[3] = Card('D',6)
-        sesh.COMMUNITY_CARDS[4] = Card('C',10)
-    elif testCode == 9: # straight flush test - double card value numbers
-        sesh.players[0].hand[0] = Card('D',10)
-        sesh.players[0].hand[1] = Card('D',8)
-        sesh.players[1].hand[0] = Card('C',6)
-        sesh.players[1].hand[1] = Card('C',2)
-
-        sesh.COMMUNITY_CARDS[0] = Card('C',3)
-        sesh.COMMUNITY_CARDS[1] = Card('C',4)
-        sesh.COMMUNITY_CARDS[2] = Card('C',5)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('C',10)
-    elif testCode == 10: # royal flush test 1
-        sesh.players[0].hand[0] = Card('D',10)
-        sesh.players[0].hand[1] = Card('D',12)
-        sesh.players[1].hand[0] = Card('C',6)
-        sesh.players[1].hand[1] = Card('C',2)
-
-        sesh.COMMUNITY_CARDS[0] = Card('D',14)
-        sesh.COMMUNITY_CARDS[1] = Card('D',11)
-        sesh.COMMUNITY_CARDS[2] = Card('D',2)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('D',13)
-
-    elif testCode == 11: # royal flush test 2
-        sesh.players[0].hand[0] = Card('D',10)
-        sesh.players[0].hand[1] = Card('D',12)
-        sesh.players[1].hand[0] = Card('C',8)
-        sesh.players[1].hand[1] = Card('C',2)
-
-        sesh.COMMUNITY_CARDS[0] = Card('D',14)
-        sesh.COMMUNITY_CARDS[1] = Card('D',11)
-        sesh.COMMUNITY_CARDS[2] = Card('C',10)
-        sesh.COMMUNITY_CARDS[3] = Card('D',7)
-        sesh.COMMUNITY_CARDS[4] = Card('D',13)
-    else:
-        testCode = 99 #play a random game
-
-    if 1 <= testCode <= 99: # use these codes for hand ranking tests
-        sesh.showState()
-        res = sesh.getBestHands()
-        sesh.logger.info(f'result: {res}')
-
-    end = time.clock()
-    print(f'Total runtime: {end-start}')
+def test_royal_flush_2():
+    sesh = Session(logger,playerCount=2)
+    players = ['Adam','Verina']
+    for i in range(len(players)):
+        sesh.players[i].name = players[i]
+    sesh.newGame()
+    sesh.players[0].hand[0] = Card('D',10)
+    sesh.players[0].hand[1] = Card('D',12)
+    sesh.players[1].hand[0] = Card('C',8)
+    sesh.players[1].hand[1] = Card('C',2)
+    sesh.COMMUNITY_CARDS[0] = Card('D',14)
+    sesh.COMMUNITY_CARDS[1] = Card('D',11)
+    sesh.COMMUNITY_CARDS[2] = Card('C',10)
+    sesh.COMMUNITY_CARDS[3] = Card('D',7)
+    sesh.COMMUNITY_CARDS[4] = Card('D',13)
+    res = sesh.getBestHands()
+    assert res[sesh.players[0]][0] == 'royal flush'
+    assert res[sesh.players[1]][0] == 'high card'
 
 if __name__ == '__main__':
-    try:
-        runTest(int(sys.argv[1]))
-    except IndexError:
-        main(0)
+    import pytest
+    pytest.main(['tests.py'])
